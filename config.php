700c2818b64792c1c36644e40da0e961feaa8a29
Jingshao Chen <jingshaochen@gmail.com>
2013-08-16 16:21:56 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("DBHOST", 'db430500328.db.1and1.com');
-define("DBNAME", 'db430500328');
-define("DBUSER", 'dbo430500328');
-define("DBPASS", '1nventoryDB');
+define("DBHOST", 'DBSERVER');
+define("DBNAME", 'DBNAME');
+define("DBUSER", 'DBUSER');
+define("DBPASS", 'DBPASS');
 ?>
